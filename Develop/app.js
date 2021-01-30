436fd23aa888bd77c2f36de38d60d1ec328cae1e
Marlee McInelly <marleemcinelly@gmail.com>
2021-01-30 08:30:11 -0700

    added git ignore, fixed dependencies installation, edits to inquirer and employee main class

--- Develop/app.js
+++ Develop/app.js
@@ -10,6 +10,45 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+// here's the inquirer code for command prompt, ends at differentiating employee types
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'Enter your name',
+            name: 'name'
+        },
+        {
+            type: 'input',
+            message: 'Enter your email',
+            name: 'email'
+        },
+        {
+            type: 'input',
+            message: 'Enter your id',
+            name: 'id'
+        },
+        {
+            type: 'list',
+            message: 'Enter your role in the company',
+            name: 'role',
+            choices: [Employee, Engineer, Intern, Manager]
+        },
+        
+    ])
+    //our contructor is gonna end up down here in .then probably
+.then((response)=>{
+    var year = new Date().getFullYear();
+    var userLicense = 'placeholder';
+    if (`${response.license}` === 'Licensed under the Apache License, Version 2.0 (the "License");\n you may not use this file except in compliance with the License.\n You may obtain a copy of the License at\n\n http://www.apache.org/licenses/LICENSE-2.0\n\n Unless required by applicable law or agreed to in writing, software\n distributed under the License is distributed on an "AS IS" BASIS,\n WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n See the License for the specific language governing permissions and\n limitations under the License.') {
+        var userLicense = `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`;
+    } else if (`${response.license}` === 'Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:\n\n 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.\n\n 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.\n\n 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.\n\n THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.'){
+        var userLicense = `![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)`;
+    } 
+    const data = userLicense + `\n\n ## ${response.title}\n\n # Description\n\n ${response.description}\n\n # Table of Contents\n\n * [Installation](#-installation)\n * [Useage](#-usage)\n * [License](#-license)\n * [Contributing](#-contributing)\n * [Tests](#-tests)\n * [Questions](#-questions)\n\n # Installation\n\n ${response.install}\n\n # Usage\n\n ${response.use}\n\n # License\n\n Copyright ` + year + ` ${response.userGitHub}\n\n ${response.license}\n\n # Contributing\n\n ${response.contribute}\n\n # Tests\n\n ${response.test}\n\n # Questions?\n\n You can find me on GitHub at: (https://github.com/${response.userGitHub})\n\n You can also email me directly at: ${response.userEmail} \n\n${response.qOrC}`;
+    fs.writeFile(`${response.title}.md`, data, (err) => err ? console.log(err) : console.log('File generated successfully! Check your folder!'));
+});
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,22 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    employeeTestLog() {
+        console.log(`employee name: ${this.name}`);
+        console.log(`employee id: ${this.id}`);
+        console.log(`employee email: ${this.email}`);
+    }
+    getName() {
+
+    }
+    getId() {
+
+    }
+    getEmail() {
+
+    }
+}
