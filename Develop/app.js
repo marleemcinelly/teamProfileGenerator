5aab470495fc6d6016ef9fb670d3be691529c6ad
Marlee McInelly <marleemcinelly@gmail.com>
2021-01-30 20:24:16 -0700

    i ahve tried my best

--- Develop/app.js
+++ Develop/app.js
@@ -10,18 +10,13 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+let employeeArray = [];
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-// // here's the inquirer code for command prompt, ends at differentiating employee types
 inquirer
     .prompt([
-        {
-            type: 'list',
-            message: 'What employee type would you like to add?',
-            name: 'role',
-            choices: [Engineer, Intern, Manager]
-        },
         {
             type: 'input',
             message: `Enter the employee's name`,
@@ -39,36 +34,94 @@ inquirer
         },
         {
             type: 'list',
-            message: 'Do you have more employees to add?',
-            name: "addMore",
-            choices: ["Yes", "No"]
-        }
-    ])
-    
-.then((response)=>{
-    // add a new employee, with type selected
-    if (`${response.addMore}` === "Yes") {
-        // a return, forcing the inquirer to loop again
-    } else {
-        // placing the writefile directly in here to allow the inquirer to exit
-        const data = `./template/main.html`; //here's where the html boilerplate goes
-        fs.writeFile(`./output/team.html`, data, (err) => err ? console.log(err) : console.log('File generated successfully! Check your folder!'));
+            message: 'What is their job title?',
+            name: 'role',
+            choices: ["Engineer", "Intern", "Manager"]
+        },
+        
+])
+ 
+.then((response) => {
+    switch(response.role) {
+        case "Engineer" :
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    message: `What's their GitHub username?`,
+                    name: 'github'
+                },
+                {
+                    type: 'list',
+                    message: 'Would you like to add another employee?',
+                    name: 'addMore',
+                    choices: ["Yes", "No"]
+                }]);
+                switch(`${response.addMore}`) { 
+                    case 'Yes' :
+                        console.log("inquirer loops")
+                        return;
+                    case 'No' :
+                        console.log("this is where your inquirer exits and lets the render function run")
+                };
+                break;
+        case "Intern" :
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    message: `What university do they attend?`,
+                    name: 'school'
+                },{
+                    type: 'list',
+                    message: 'Would you like to add another employee?',
+                    name: 'addMore',
+                    choices: ["Yes", "No"]
+                }]);
+                switch(`${response.addMore}`) { 
+                    case 'Yes' :
+                        console.log("inquirer loops")
+                        return;
+                    case 'No' :
+                        console.log("this is where your inquirer exits and lets the render function run")
+                };
+                break;
+        case "Manager" :
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    message: `What's your office number?`,
+                    name: 'officeNumber'
+                },{
+                    type: 'list',
+                    message: 'Would you like to add another employee?',
+                    name: 'addMore',
+                    choices: ["Yes", "No"]
+                }
+            ]);
+            switch(`${response.addMore}`) { 
+                case 'Yes' :
+                    console.log("inquirer loops")
+                    return;
+                case 'No' :
+                    console.log("this is where your inquirer exits and lets the render function run")
+            };
+            break;
     }
-    
-});
-
-
-
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+// generate and return a block of HTML including templated divs for each employee!  
+
+render(employeeArray);
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
+
+fs.writeFile(`./output/team.html`, render(employeeArray), (err) => err ? console.log(err) : console.log('File generated successfully! Check your folder!'));
+    
+});
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
